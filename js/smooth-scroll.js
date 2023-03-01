c0a8e04aa96391ab78ea81060f840dd008f3f6a6
Bohdan Khomych <bod.khomych@gmail.com>
2023-03-01 12:25:58 +0200

    fix a bug with mobile-menu

--- js/smooth-scroll.js
+++ js/smooth-scroll.js
@@ -10,9 +10,15 @@ function onAnchorClick(evt) {
 
   const targetedEl = document.querySelector(`${evt.target.getAttribute('href')}`);
   const targetedElCoordinates = targetedEl.offsetTop;
+  const mobileMenu = document.querySelector('.js-menu-container');
 
   window.scrollTo({
     top: targetedElCoordinates,
     behavior: 'smooth',
   });
+
+  if (mobileMenu.classList.contains('is-open')) {
+    mobileMenu.classList.remove('is-open');
+    document.body.classList.remove('no-scroll');
+  }
 }
